70bca7abaf897fd7fc3a3ef459c724e1b88ed3cb
HarryB05 <49901436+HarryB05@users.noreply.github.com>
2025-04-25 22:49:45 +0100

    text to fix deployment bug on pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/harryb05.github.io',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
